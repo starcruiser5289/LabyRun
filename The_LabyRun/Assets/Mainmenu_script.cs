a65c41b3e1bcb7baab70e6c5b5e8952a7631f4fb
mohamednaelguermi <55802422+mohamednaelguermi@users.noreply.github.com>
2020-04-20 20:58:32 +0200

    Mainmenu_Nail

---
+++ The_LabyRun/Assets/Mainmenu_script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu_script : MonoBehaviour
+{
+    public void jouer()
+    {
+        SceneManager.LoadScene("Scenes/Level 1"); 
+    }
+
+    public void quitter()
+    {
+        Application.Quit();
+    }
+}
